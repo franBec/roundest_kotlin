edd2caaa71b8366240b68e86ae6c63fd3bf1d864
Fran Bec <franbecvort@gmail.com>
2024-11-28 20:32:11 +0000

    initial commit

---
+++ src/main/kotlin/dev/pollito/roundest_kotlin/RoundestKotlinApplication.kt
@@ -0,0 +1,11 @@
+package dev.pollito.roundest_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoundestKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<RoundestKotlinApplication>(*args)
+}
