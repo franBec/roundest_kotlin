39bb2a61fb1fd1b18f5b3640ae95760c9c516a57
Fran Bec <franbecvort@gmail.com>
2024-12-29 16:09:48 +0000

    fabrikt default sort needs preprocessing to not break combinedSort logic

--- src/main/kotlin/dev/pollito/roundest_kotlin/util/PageableUtils.kt
+++ src/main/kotlin/dev/pollito/roundest_kotlin/util/PageableUtils.kt
@@ -4,13 +4,22 @@ import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 
+private const val DEFAULT_SORT_PROPERTY = "id"
+
 object PageableUtils {
 
   fun createPageable(page: Int, size: Int, sort: List<String>): Pageable {
-    var combinedSort: Sort = Sort.unsorted()
-    var hasIdSort = false
+    val processedSort =
+        if (sort.size == 1 && sort[0] == "[$DEFAULT_SORT_PROPERTY:asc]") {
+          listOf(DEFAULT_SORT_PROPERTY)
+        } else {
+          sort
+        }
 
-    for (sortField in sort) {
+    var combinedSort: Sort = Sort.unsorted()
+    var hasDefaultSortProperty = false
+
+    for (sortField in processedSort) {
       val sortParams = sortField.split(":")
       val direction =
           if (sortParams.size > 1 && sortParams[1].equals("desc", ignoreCase = true)) {
@@ -19,15 +28,15 @@ object PageableUtils {
             Sort.Direction.ASC
           }
 
-      if (sortParams[0].equals("id", ignoreCase = true)) {
-        hasIdSort = true
+      if (sortParams[0].equals(DEFAULT_SORT_PROPERTY, ignoreCase = true)) {
+        hasDefaultSortProperty = true
       }
 
       combinedSort = combinedSort.and(Sort.by(direction, sortParams[0]))
     }
 
-    if (!hasIdSort) {
-      combinedSort = combinedSort.and(Sort.by(Sort.Direction.ASC, "id"))
+    if (!hasDefaultSortProperty) {
+      combinedSort = combinedSort.and(Sort.by(Sort.Direction.ASC, DEFAULT_SORT_PROPERTY))
     }
 
     return PageRequest.of(page, size, combinedSort)
